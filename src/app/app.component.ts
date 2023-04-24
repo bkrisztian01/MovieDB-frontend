697ce8fb670fb802444e8bbe883ba7292354c745
bkrisztian01 <bui.krisztian01@gmail.com>
2023-04-24 13:29:49 +0200

    Setup eslint, prettier and env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'moviedb';
+
+  constructor() {
+    console.log(environment.THEMOVIEDB_API_KEY);
+  }
 }

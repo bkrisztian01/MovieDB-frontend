7b71db2dbf6eb1f7cd1a0c59562b0c55ff7fd9dc
bkrisztian01 <bui.krisztian01@gmail.com>
2023-05-08 22:44:16 +0200

    Discover TV shows page

--- src/app/components/discover-tv-shows/discover-tv-shows.component.ts
+++ src/app/components/discover-tv-shows/discover-tv-shows.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MovieItem } from 'src/app/models/MovieItem';
+import { DiscoverResult } from 'src/app/models/SearchResult';
+import { Show } from 'src/app/models/Show';
+import { MovieDbService } from 'src/app/services/movie-db.service';
 
 @Component({
   selector: 'app-discover-tv-shows',
   templateUrl: './discover-tv-shows.component.html',
-  styleUrls: ['./discover-tv-shows.component.scss']
+  styleUrls: ['./discover-tv-shows.component.scss'],
 })
-export class DiscoverTvShowsComponent {
+export class DiscoverTvShowsComponent implements OnInit {
+  discoverShowsResult: DiscoverResult<Show>;
+  page: number;
 
+  constructor(
+    private movieDbService: MovieDbService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((queries) => {
+      const pageNumber = queries.get('page');
+      this.page = Number(pageNumber) || 1;
+
+      this.movieDbService.getDiscoverShows(this.page).subscribe((data) => {
+        this.discoverShowsResult = data;
+      });
+    });
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.page = e.pageIndex + 1;
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { ...this.route.snapshot.queryParams, page: this.page },
+    });
+  }
 }
--- src/app/components/movie-item/movie-item.component.ts
+++ src/app/components/movie-item/movie-item.component.ts
@@ -1,19 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { MovieItem } from 'src/app/models/MovieItem';
 
 @Component({
-  selector: 'app-movie-item',
+  selector:
+    'app-movie-item[id][title][posterPath][voteAverage][itemPathPrefix]',
   templateUrl: './movie-item.component.html',
   styleUrls: ['./movie-item.component.scss'],
 })
 export class MovieItemComponent {
-  @Input() movie: MovieItem;
+  @Input() id: number;
+  @Input() title: string;
+  @Input() posterPath: string;
+  @Input() voteAverage: number;
   @Input() itemPathPrefix: string;
 
   constructor(private router: Router) {}
 
   onClick() {
-    this.router.navigate([this.itemPathPrefix, this.movie.id]);
+    this.router.navigate([this.itemPathPrefix, this.id]);
   }
 }
